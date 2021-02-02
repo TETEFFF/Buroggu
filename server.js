69164ceddaac6af18a8daa62427844f21c4bf1b6
TETEFFF <abdellatif.hagrouf@gmail.com>
2021-02-02 20:31:01 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,34 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').parse()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+ //import routes
+const indexRouter = require('./routes/index')
+
+
+
+
+// I dunno what this block underneath does 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+// // // //
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to mongoose'))
+
+//use routes
+app.use('/', indexRouter)
+
+
+//Listening in port
+app.listen(process.env.PORT || 6009)
